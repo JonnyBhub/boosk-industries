c53197e888734137f953dc9201044f0ec80ddfd1
JBSopra <jonathan.barrett@soprasteria.com>
2023-10-20 23:51:46 +0100

    added materialui

--- boosk-app/src/index.jsx
+++ boosk-app/src/index.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Textbox from './components/textbox';
+import { TextField } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Textbox />
+    <App>
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </App>
   </React.StrictMode>
 );
 
