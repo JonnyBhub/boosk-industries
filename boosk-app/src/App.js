663a0eb8dc3faabca33467a55ae87db339d7092e
JBSopra <jonathan.barrett@soprasteria.com>
2023-10-21 14:56:47 +0100

    default input

--- boosk-app/src/App.js
+++ boosk-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Input} from '@aws-amplify/ui-react';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Input></Input>
         <a
           className="App-link"
           href="https://reactjs.org"
