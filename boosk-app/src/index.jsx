9de96ead41ac3718a0b5a4b293cf5f9ae423fc84
JBSopra <jonathan.barrett@soprasteria.com>
2023-10-21 12:43:08 +0100

    text from aplify library

--- boosk-app/src/index.jsx
+++ boosk-app/src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Text } from '@aws-amplify/ui-react'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App>
+      <Text>Hello, Amplify!</Text>
     </App>
   </React.StrictMode>
 );
