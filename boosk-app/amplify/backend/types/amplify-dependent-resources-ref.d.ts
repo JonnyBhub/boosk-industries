b0d4562a1ed6d10adb669116d361ad56f955ab36
JBSopra <jonathan.barrett@soprasteria.com>
2023-10-21 15:29:09 +0100

    amplify changes

---
+++ boosk-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "booskindustriesbooskd8b2ce73": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
